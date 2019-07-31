cd4453b30e764cbe1aa28a763bd74c01a667585d
ishareef7 <31832217+ishareef7@users.noreply.github.com>
2019-07-31 18:18:57 -0400

    Updated model training notebooks and build dataset script

--- Capstone2/src/features/build_dataset.py
+++ Capstone2/src/features/build_dataset.py
@@ -3,6 +3,9 @@ import numpy as np
 import ast
 import os
 
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+
 # path to folder that contains the raw data files
 data_file_path = '/Users/ishareef7/Springboard/Capstone2/data/raw/fma_metadata'
 
@@ -86,7 +89,10 @@ echonest_features.columns = ['_'.join(col).rstrip('_') for col in echonest_featu
 
 
 dataset_echonest = features.join(echonest_features, how = 'inner').join(track_info, how = 'inner')
-dataset_no_echnoest = features.join(track_info, how = 'inner')
+dataset_no_echnoest = features.join(track_info, how = 'inner').dropna()
+
+dataset_echonest['genre_top'] = dataset_echonest['genre_top'].astype('int64')
+dataset_no_echnoest['genre_top'] =dataset_no_echnoest['genre_top'].astype('int64')
 
 def get_dataset_echonest():
     """Return dataset that contains collumns for echonest features this dataset only contains 13,129 rows """
@@ -95,3 +101,50 @@ def get_dataset_echonest():
 def get_dataset():
     """ Return dataset without echonest features this dataset contains 106,574 rows"""
     return dataset_no_echnoest
+
+
+def process_dataset(df, validation = False):
+    """Extract and scale features of inpyted dataframe. Encode output labels and split into training and test sets"""
+    features = df.iloc[:, :-5].columns
+    y = df['genre_top']
+
+    if validation == True:
+        train = df['split'] == 'training'
+        test = df['split'] == 'test'
+        val = df['split'] == 'validation'
+
+        X_train = df.loc[train, features]
+        X_test = df.loc[test, features]
+        X_val = df.loc[val, features]
+
+        # Create Encoder instance and encode train and test labels
+        enc = LabelEncoder()
+        y_train = enc.fit_transform(y[train])
+        y_test = enc.transform(y[test])
+        y_val = enc.transform(y[val])
+
+        # Create scaler instance and transform train and test features
+        scaler = StandardScaler(copy=False)
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+        X_val = scaler.transform(X_val)
+
+        return X_train, X_test, X_val, y_train, y_test, y_val
+    else:
+        train = (df['split'] == 'training') | (df['split'] == 'validation')
+        test = df['split'] == 'test'
+
+        X_train = df.loc[train, features]
+        X_test = df.loc[test, features]
+
+        # Create Encoder instance and encode train and test labels
+        enc = LabelEncoder()
+        y_train = enc.fit_transform(y[train])
+        y_test = enc.transform(y[test])
+
+        # Create scaler instance and transform train and test features
+        scaler = StandardScaler(copy=False)
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        return X_train, X_test, y_train, y_test
